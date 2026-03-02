fb4a168c9d061e1628ac7568b7a9f572a35ec3cd
Stephen Melnick <stephenwmelnick@gmail.com>
2026-03-02 13:35:47 -0500

    Split gamemode into state machine and core

--- Game/GameMode.cs
+++ Game/GameMode.cs
@@ -18,16 +18,19 @@ public partial class GameMode : Node
 	
 	public Match3RecipeTable recipes { get; private set; }
 
-	private readonly List<SpawnLocation> _locations = [];
-	public readonly List<Node2D> players = [];
-	private readonly List<PlayerState> _playerStates = [];
-	
-	public Node worldRoot { get; private set; }
-
 	public CraterEvent<int, Node2D> onPlayerSpawned = new ();
+	public Node worldRoot { get; private set; }
+	public readonly List<Node2D> players = [];
 	
 	// Serialized settings, because this is a singleton
-	private GameModeSettings _settings;
+	public GameModeSettings settings { get; private set; }
+	
+	
+	public readonly List<SpawnLocation> spawnLocations = [];
+	public readonly List<PlayerState> playerStates = [];
+	
+	private GameState _currentGameState;
+	private VersusGameState versusGameState { get; } = new();
 
 	public override void _EnterTree()
 	{
@@ -39,35 +42,25 @@ public partial class GameMode : Node
 		statusEffects = ResourceLoader.Load<StatusEffectList>("res://Game/Effects/SL_Effects.tres");
 		recipes = ResourceLoader.Load<Match3RecipeTable>("res://Game/Match3/Recipes/M3t_Default.tres");
 		ImGuiGodot.ImGuiGD.ToolInit();
-		
-		var ingredients0 = new List<MatchType> { MatchType.Fire, MatchType.Fire };
-		var ingredients1 = new List<MatchType> { MatchType.Fire, MatchType.Water };
-		var ingredients2 = new List<MatchType> { MatchType.Fire, MatchType.Fire, MatchType.Fire };
-		var ingredients3 = new List<MatchType> { MatchType.Fire, MatchType.Fire, MatchType.Fire, MatchType.Fire };
 
-		GD.Print($"Loaded {recipes.count} recipes.");
-		
-		GD.Print($"Make recipe '{string.Join(", ", ingredients0)}': {recipes.GetEnemy(ingredients0)}");
-		GD.Print($"Make recipe '{string.Join(", ", ingredients1)}': {recipes.GetEnemy(ingredients1)}");
-		GD.Print($"Make recipe '{string.Join(", ", ingredients2)}': {recipes.GetEnemy(ingredients2)}");
-		GD.Print($"Make recipe '{string.Join(", ", ingredients3)}': {recipes.GetEnemy(ingredients3)}");
+		settings = ResourceLoader.Load<GameModeSettings>("res://Game/DefaultSettings.tres");
 
-		_settings = ResourceLoader.Load<GameModeSettings>("res://Game/DefaultSettings.tres");
-		SpawnPlayers();
+		_currentGameState = versusGameState;
+		versusGameState.EnterState(this);
 
-		worldRoot = _locations[0].Owner;
+		worldRoot = spawnLocations[0].Owner;
 	}
 
 	public void AddSpawnLocation(SpawnLocation location)
 	{
-		_locations.Insert((int)location.defaultIndex, location);
+		spawnLocations.Insert((int)location.defaultIndex, location);
 	}
 
 	public PlayerState GetPlayerState(int playerIndex)
 	{
-		if (playerIndex >= 0 && playerIndex < _playerStates.Count)
+		if (playerIndex >= 0 && playerIndex < playerStates.Count)
 		{
-			return _playerStates[playerIndex];
+			return playerStates[playerIndex];
 		}
 		
 		GD.PrintErr("[GameMode] Attempted to access a player state from an out of bounds index.");
@@ -76,60 +69,10 @@ public partial class GameMode : Node
 
 	public void NotifyGemDestroyed(int destroyerPlayerIndex, Vector2 offset)
 	{
-		_playerStates[GetRivalIndex(destroyerPlayerIndex)].match3Spawner.QueueRelativeSpawn(_settings.gem, offset);
+		_currentGameState.NotifyGemDestroyed(this, destroyerPlayerIndex, offset);
 	}
 
-	private void SpawnPlayers()
-	{
-		var remainingSpawnLocations = new List<SpawnLocation>(_locations);
-		for (var i = 0; i < _settings.playerCount; ++i)
-		{
-			GD.Print($"[GameMode] Spawning player {i}...");
-			var chosenSpawnIndex = GD.RandRange(0, remainingSpawnLocations.Count - 1);
-			var spawnLocation = remainingSpawnLocations[chosenSpawnIndex];
-			var playerInstance = _settings.player.Instantiate<Node2D>();
-			playerInstance.SetGlobalPosition(spawnLocation.GlobalPosition);
-			playerInstance.Name = $"Player{i}";
-			spawnLocation.Owner.AddChild(playerInstance);
-			remainingSpawnLocations.RemoveAt(chosenSpawnIndex);
-			
-			players.Add(playerInstance);
-			CraterFunctions.GetNodeByClass<PlayerController>(playerInstance)?.BindInput(i);
-
-			onPlayerSpawned.Invoke(i, playerInstance);
-
-			var playerState = CraterFunctions.GetNodeByClass<PlayerState>(playerInstance);
-			if (playerState == null)
-			{
-				continue;
-			}
-			
-			_playerStates.Add(playerState);
-			var i1 = i;
-			playerState.container.onSpawnSingleRequested.AddListener(orb => SpawnEnemyFromSingleType(i1, orb));
-			playerState.container.onSpawnRequested.AddListener(orbs => SpawnEnemyFromMatch3(i1, orbs));
-			playerState.index = i;
-		}
-	}
-
-	private void SpawnEnemyFromMatch3(int ownerIndex, List<MatchType> orbElements)
-	{
-		var spawner = _playerStates[GetRivalIndex(ownerIndex)].match3Spawner;
-		// spawner.QueueSpawn();
-	}
-
-	private void SpawnEnemyFromSingleType(int ownerIndex, MatchType orbType)
-	{
-		var enemyInstance = recipes.GetEnemyFromOrbType(orbType);
-		if (enemyInstance == null)
-		{
-			return;
-		}
-		
-		_playerStates[GetRivalIndex(ownerIndex)].match3Spawner.QueueSpawn(enemyInstance);
-	}
-
-	private static int GetRivalIndex(int playerIndex)
+	public static int GetRivalIndex(int playerIndex)
 	{
 		return playerIndex switch
 		{
---
+++ Game/GameState.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using CraterSprite.Match3;
+using Godot;
+
+namespace CraterSprite;
+
+public abstract class GameState
+{
+    public abstract void EnterState(GameMode mode);
+
+    public abstract void ExitState();
+    
+    public abstract void NotifyGemDestroyed(GameMode mode, int destroyerPlayerIndex, Vector2 offset);
+}
+
+public class VersusGameState : GameState
+{
+    public override void EnterState(GameMode mode)
+    {
+    }
+
+    public override void ExitState()
+    {
+    }
+
+    public override void NotifyGemDestroyed(GameMode mode, int destroyerPlayerIndex, Vector2 offset)
+    {
+        mode.GetPlayerState(GameMode.GetRivalIndex(destroyerPlayerIndex)).match3Spawner.QueueRelativeSpawn(mode.settings.gem, offset);
+    }
+    
+    private void SpawnPlayers(GameMode mode)
+    {
+        var remainingSpawnLocations = new List<SpawnLocation>(mode.spawnLocations);
+        for (var i = 0; i < mode.settings.playerCount; ++i)
+        {
+            GD.Print($"[GameMode] Spawning player {i}...");
+            var chosenSpawnIndex = GD.RandRange(0, remainingSpawnLocations.Count - 1);
+            var spawnLocation = remainingSpawnLocations[chosenSpawnIndex];
+            var playerInstance = mode.settings.player.Instantiate<Node2D>();
+            playerInstance.SetGlobalPosition(spawnLocation.GlobalPosition);
+            playerInstance.Name = $"Player{i}";
+            spawnLocation.Owner.AddChild(playerInstance);
+            remainingSpawnLocations.RemoveAt(chosenSpawnIndex);
+			
+            mode.players.Add(playerInstance);
+            CraterFunctions.GetNodeByClass<PlayerController>(playerInstance)?.BindInput(i);
+
+            mode.onPlayerSpawned.Invoke(i, playerInstance);
+
+            var playerState = CraterFunctions.GetNodeByClass<PlayerState>(playerInstance);
+            if (playerState == null)
+            {
+                continue;
+            }
+			
+            mode.playerStates.Add(playerState);
+            var i1 = i;
+            playerState.container.onSpawnSingleRequested.AddListener(orb => SpawnEnemyFromSingleType(mode, i1, orb));
+            playerState.container.onSpawnRequested.AddListener(orbs => SpawnEnemyFromMatch3(mode, i1, orbs));
+            playerState.index = i;
+        }
+    }
+    
+    private void SpawnEnemyFromMatch3(GameMode mode, int ownerIndex, List<MatchType> orbElements)
+    {
+        var spawner = mode.GetPlayerState(GameMode.GetRivalIndex(ownerIndex)).match3Spawner;
+        // spawner.QueueSpawn();
+    }
+
+    private void SpawnEnemyFromSingleType(GameMode mode, int ownerIndex, MatchType orbType)
+    {
+        var enemyInstance = mode.recipes.GetEnemyFromOrbType(orbType);
+        if (enemyInstance == null)
+        {
+            return;
+        }
+        var spawner = mode.GetPlayerState(GameMode.GetRivalIndex(ownerIndex)).match3Spawner;
+        spawner.QueueSpawn(enemyInstance);
+    }
+}
