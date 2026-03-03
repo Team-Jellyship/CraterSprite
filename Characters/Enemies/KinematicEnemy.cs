610616d28e6c455c57050ceee05e2e34d33c5120
Tiller99c <tylerforrester9910@gmail.com>
2026-03-02 21:08:07 -0500

    Updated Enemy
    The Enemy walks, avoids the ledges and stops if it sees a player, this is good news.

--- Characters/Enemies/KinematicEnemy.cs
+++ Characters/Enemies/KinematicEnemy.cs
@@ -37,50 +37,79 @@ namespace CraterSprite
 		[Export(PropertyHint.None, "suffix:g")]
 		private float _gravity = 1.0f;
 		
-		private float _movingDirection = 1.0f;
+		private float _facingDirection = 1.0f;
 		
 		public float moveInput { get; private set; }
 		
 		public override void _PhysicsProcess(double delta)
 		{
+			var rayCast = GetNode<RayCast2D>("PlayerRay");
+			var groundCast = GetNode<RayCast2D>("GroundRay");
+			var attackTimer = GetNode<Timer>("AttackTimer");
+			//rayCast.CollideWithAreas = true;
 			//Variable used to store the Velocity
 			var currentVelocity = Velocity;
+			//Code that turns the enemy around
 			if(IsOnWall())
 			{
-				_movingDirection = -_movingDirection;
+				_facingDirection = -_facingDirection;
+				if(_facingDirection < 0){
+					rayCast.SetTargetPosition(new Vector2(-50,0));
+					groundCast.SetTargetPosition(new Vector2(-20,20));
+				}
+				else{
+					rayCast.SetTargetPosition(new Vector2(50,0));
+					groundCast.SetTargetPosition(new Vector2(20,20));
+				}
+				
 			}
+			
+			//Determines if the entitiy is going to walk off an edge and turns them
+			Object groundDetect = groundCast.GetCollider();
+			if(IsOnFloor()){
+				if(groundDetect is Node groundFound){
+					//Fuck my brain is off right now I don't want to figure out the negative of this
+				}
+				else{
+					_facingDirection = -_facingDirection;
+					if(_facingDirection < 0){
+						rayCast.SetTargetPosition(new Vector2(-50,0));
+						groundCast.SetTargetPosition(new Vector2(-20,20));
+					}
+					else{
+						rayCast.SetTargetPosition(new Vector2(50,0));
+						groundCast.SetTargetPosition(new Vector2(20,20));
+					}
+				}
+			}
+			
 			//Applies gravity when enemy is not on ground
 			if(!IsOnFloor()){
 				currentVelocity.Y += GravityConstant * _gravity * (float)delta;
 			}
-			
-			currentVelocity.X += _movingDirection * _acceleration;
+			currentVelocity.X += _facingDirection * _acceleration;
 			//Sets a max Speed
 			if(Math.Abs(currentVelocity.X) > _maxSpeed){
-				currentVelocity.X = _maxSpeed * _movingDirection;
+				currentVelocity.X = _maxSpeed * _facingDirection;
 			}
-			
-			var rayCast = GetNode<RayCast2D>("PlayerRay");
-			
-			if(rayCast.IsColliding()){
-				//currentVelocity.X -= _movingDirection * _acceleration;
-				//if(Math.Abs(currentVelocity.X) < 0){
-				//	currentVelocity.X = 0.0f;
-				//}
-				currentVelocity.X = 0.0f;
-				
+
+			Object enemyInMySight = rayCast.GetCollider();
+			if(enemyInMySight is Node collidedNode){
+				if(string.Equals(collidedNode.GetParent().Name, "Player")){
+					currentVelocity.X = 0;
+				}
+				//Fuck my stupid chungus life this feels so Pirate Software Coded
+				if(string.Equals(collidedNode.GetParent().Name, "@CharacterBody2D@5")){
+					currentVelocity.X = 0;
+				}
 			}
-			
-			
-			GD.Print(currentVelocity.X);
-			
+
 			//Sets our temporary Variable into active in the game
 			SetVelocity(currentVelocity);
-			
+			//Draws the Raycast Arrows
+			QueueRedraw();
 			//Physical Engine goes BRRRRR
 			MoveAndSlide();
-			
-			
 		}
 		
 		public void SetMoveInput(float input)
@@ -91,7 +120,9 @@ namespace CraterSprite
 		
 		public override void _Draw()
 		{
-			DebugHelpers.Drawing.DrawArrow(this, Vector2.Zero, GetVelocity() * 0.25f, new Color(1.0f, 0.0f, 0.0f));
+			//var temp = GetVelocity();
+			DebugHelpers.Drawing.DrawArrow(this, Vector2.Zero, GetNode<RayCast2D>("PlayerRay").GetTargetPosition(), new Color(1.0f, 0.0f, 0.0f));
+			DebugHelpers.Drawing.DrawArrow(this, Vector2.Zero, GetNode<RayCast2D>("GroundRay").GetTargetPosition(), new Color(1.0f, 0.0f, 1.0f));
 		}
 	}
 }
