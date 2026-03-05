d0c605f4baf0e38394e1e76adaafa7b3c45d2c7e
Stephen Melnick <stephenwmelnick@gmail.com>
2026-03-05 16:23:04 -0500

    Made player hud into a scene with PlayerHudRoot.cs as its root class

---
+++ UI/HUD/Scripts/PlayerHudRoot.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace CraterSprite.UI.HUD.Scripts;
+
+public partial class PlayerHudRoot : Control
+{
+    [Export] private int _playerIndex;
+    
+    public override void _EnterTree()
+    {
+        ChildEnteredTree += (node =>
+        {
+            node.Set("_playerIndex", _playerIndex);
+        });
+    }
+}
