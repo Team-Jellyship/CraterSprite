3185ebefd14fa3d93a621b6ec60748c95c5307e7
Stephen Melnick <stephenwmelnick@gmail.com>
2026-03-09 14:12:09 -0400

    Added health display to player HUD

---
+++ UI/HUD/Scripts/PlayerHealthDisplay.cs
@@ -0,0 +1,20 @@
+﻿using CraterSprite.Game.GameMode;
+using Godot;
+
+namespace CraterSprite.UI.HUD.Scripts;
+
+public partial class PlayerHealthDisplay : AutobindUiElement
+{
+    [Export] private Label _healthText;
+    protected override void Bind(PlayerState playerState)
+    {
+        playerState.RegisterEffectChangedDelegate(GameMode.instance.statusEffects.health, HealthChanged, this);
+        var currentHealth = playerState.GetEffectValue(GameMode.instance.statusEffects.health);
+        _healthText.Text =$"hp: {currentHealth}";
+    }
+
+    private void HealthChanged(int stacks, float newHealth)
+    {
+        _healthText.Text = $"hp: {newHealth}";
+    }
+}
