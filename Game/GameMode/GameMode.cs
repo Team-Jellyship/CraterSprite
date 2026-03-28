61ce6c22765008d0ca54cbf3e87081c61808d2dd
Paul <aliceapa111@gmail.com>
2026-03-28 17:47:11 -0400

    Main Menu Created

--- Game/GameMode/GameMode.cs
+++ Game/GameMode/GameMode.cs
@@ -107,7 +107,7 @@ public partial class GameMode : Node
 		{ return playerData.TrueForAll(data => data.playerScore < settings.roundsToWin) ? loadingState : rematchState; });
 		_transitions.Add(new Tuple<GameState, GameModeCommand>(rematchState, GameModeCommand.Victory), () => loadingState);
 		
-		_currentGameState = loadingState;
+		_currentGameState = characterSelectState;
 		_currentGameState.EnterState(this);
 		if (_currentGameState.transitionTime <= 0.0f)
 		{
